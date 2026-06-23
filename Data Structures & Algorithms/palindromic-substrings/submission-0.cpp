ccb6318c0ff52c083319d5c5a565db402fcaa93d
Shreyas Nayak <shreyasnayak0220@gmail.com>
2026-06-23 11:07:52 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int l = 0, r = 0;
+        int ans = 0 ;
+
+        for (int i = 0; i < s.size(); i++) {
+            int left = i, right = i;
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                ans++ ;
+                left--;
+                right++;
+            }
+
+            left = i;
+            right = i + 1;
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                ans++ ;
+                left--;
+                right++;
+            }
+        }
+
+        return ans;
+    }
+};
